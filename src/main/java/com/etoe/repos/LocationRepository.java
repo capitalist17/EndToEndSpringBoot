afac8a468c974a5f7708b0fd1cf7c45cd8605bd3
Arvind SN <asn@asn.com>
2019-02-18 16:49:23 +0530

    Implement the Service methods

--- src/main/java/com/etoe/repos/LocationRepository.java
+++ src/main/java/com/etoe/repos/LocationRepository.java
@@ -1,9 +1,11 @@
 package com.etoe.repos;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.etoe.entities.Location;
 
-public interface LocationRepository extends CrudRepository<Location, Integer>{
+//Change this from CRUDRepository to JpaRepository as findAll method in CRUDRepository returns an iterable.
+//If you want a method that returns a List then use JpaRepository. JpaRepository is a sub interface of CRUDRepo.
+public interface LocationRepository extends JpaRepository<Location, Integer>{
 
 }
--- src/main/java/com/etoe/services/LocationServiceImpl.java
+++ src/main/java/com/etoe/services/LocationServiceImpl.java
@@ -2,38 +2,55 @@ package com.etoe.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.etoe.entities.Location;
+import com.etoe.repos.LocationRepository;
+/*
+ * @Service specialized form of @Component annotation. Indicates the purpose for which the bean is instantiated.
+ * Spring @Service annotation is used with classes that provide some business functionalities. Spring context 
+ * will auto-detect these classes when annotation-based configuration and classpath scanning is used.
+ * */
 
+@Service
 public class LocationServiceImpl implements LocationService {
-
+	
+	@Autowired
+	private  LocationRepository repository;
+	
 	@Override
 	public Location saveLocation(Location location) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(location);
 	}
 
 	@Override
 	public Location updateLocation(Location location) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(location);
 	}
 
 	@Override
 	public void deleteLocation(Location location) {
-		// TODO Auto-generated method stub
-		
+		repository.delete(location);
 	}
 
 	@Override
 	public Location getLocationById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.findById(id).get();
 	}
 
 	@Override
 	public List<Location> getAllLocations() {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.findAll();
 	}
 
+	public LocationRepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(LocationRepository repository) {
+		this.repository = repository;
+	}
+	
+	
 }
