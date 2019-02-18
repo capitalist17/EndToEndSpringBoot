20568d065732b323e876002a275662a75f1c621c
Arvind SN <asn@asn.com>
2019-02-18 16:15:09 +0530

    Create the Location Repository

---
+++ src/main/java/com/etoe/repos/LocationRepository.java
@@ -0,0 +1,9 @@
+package com.etoe.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.etoe.entities.Location;
+
+public interface LocationRepository extends CrudRepository<Location, Integer>{
+
+}
