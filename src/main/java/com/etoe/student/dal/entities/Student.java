af13660176067a39cebc6d769046a65ca91f82e0
Arvind SN <asn@asn.com>
2019-02-18 14:19:32 +0530

    Add auto increment capability to the entity object Student

--- src/main/java/com/etoe/student/dal/entities/Student.java
+++ src/main/java/com/etoe/student/dal/entities/Student.java
@@ -2,6 +2,8 @@ package com.etoe.student.dal.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 @Table(name = "studenttab") // If the table in DB were to be named as Student, then this annotation is not required.
 public class Student {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	@Column(name = "sname")
 	private String name;
