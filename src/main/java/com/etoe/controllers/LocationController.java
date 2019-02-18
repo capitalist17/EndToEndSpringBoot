296b873c1203ffec1082fbf0f065de7e76905a16
Arvind SN <asn@asn.com>
2019-02-18 18:39:30 +0530

    Saving the location to the DB and returing the ID to the view after successful save

--- src/main/java/com/etoe/controllers/LocationController.java
+++ src/main/java/com/etoe/controllers/LocationController.java
@@ -1,12 +1,29 @@
 package com.etoe.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.etoe.entities.Location;
+import com.etoe.services.LocationService;
 
 @Controller
 public class LocationController {
+	@Autowired
+	LocationService locService;
+	
 	@RequestMapping("/showCreate")
 	public String showCreate() {
+		return "createLocation";
+	}
+	
+	@RequestMapping("/saveLoc")
+	public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap ) {
+		Location savedLoc = locService.saveLocation(location);
+		String msg = "Location saved with id: " + savedLoc.getId();
+		modelMap.addAttribute("msg", msg);
 		return "createLocation";
 	}
 }
