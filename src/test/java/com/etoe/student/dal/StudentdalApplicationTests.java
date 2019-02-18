2c0b08272b0ed242d14c8548213cd901fc6cedeb
Arvind SN <asn@asn.com>
2019-02-18 14:31:23 +0530

    Create a test student in DB to check if all the DB wiring is good

--- src/test/java/com/etoe/student/dal/StudentdalApplicationTests.java
+++ src/test/java/com/etoe/student/dal/StudentdalApplicationTests.java
@@ -2,15 +2,28 @@ package com.etoe.student.dal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.etoe.student.dal.entities.Student;
+import com.etoe.student.dal.repos.StudentRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentdalApplicationTests {
-
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	@Test
-	public void contextLoads() {
+	public void testCreateStudent() {
+		Student student = new Student();
+		student.setName("John Doe");
+		student.setCourse("Java Web Services");
+		student.setFee(30d);
+		studentRepository.save(student);
 	}
+	
 
 }
