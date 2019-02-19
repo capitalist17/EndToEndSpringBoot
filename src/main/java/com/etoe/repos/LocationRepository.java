3a988a09d72cc0927363d62cbd7c6a93a9d4d056
Arvind SN <asn@asn.com>
2019-02-19 20:29:26 +0530

    Chapter07 is also present here - report utility

--- src/main/java/com/etoe/repos/LocationRepository.java
+++ src/main/java/com/etoe/repos/LocationRepository.java
@@ -1,11 +1,16 @@
 package com.etoe.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.etoe.entities.Location;
 
 //Change this from CRUDRepository to JpaRepository as findAll method in CRUDRepository returns an iterable.
 //If you want a method that returns a List then use JpaRepository. JpaRepository is a sub interface of CRUDRepo.
-public interface LocationRepository extends JpaRepository<Location, Long>{
 
+public interface LocationRepository extends JpaRepository<Location, Long>{
+	@Query(value="select type,count(type) from projectdb.location group by type", nativeQuery = true)
+	public List<Object[]> findTypeAndTypeCount();
 }
---
+++ src/main/java/com/etoe/util/ReportUtil.java
@@ -0,0 +1,6 @@
+package com.etoe.util;
+
+import java.util.List;
+public interface ReportUtil {
+	void generatePieChart(String path, List<Object[]> data);
+}
