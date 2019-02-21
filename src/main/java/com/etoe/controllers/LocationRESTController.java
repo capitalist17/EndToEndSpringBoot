5e9ae2f50c51da08568fcd6cd41584c5caccb223
Arvind SN <asn@asn.com>
2019-02-21 12:05:23 +0530

    Get all locations

--- src/main/java/com/etoe/controllers/LocationRESTController.java
+++ src/main/java/com/etoe/controllers/LocationRESTController.java
@@ -1,10 +1,24 @@
 package com.etoe.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.etoe.entities.Location;
+import com.etoe.repos.LocationRepository;
 
 @RestController
 @RequestMapping("/locations")
 public class LocationRESTController {
-
+	
+	@Autowired
+	LocationRepository locRepo;
+	
+	@GetMapping
+	public List<Location> getLocations(){
+		return locRepo.findAll();
+	}
 }
