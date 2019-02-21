10ffc30ddf794fc489e100803be0dba971cf8bac
Arvind SN <asn@asn.com>
2019-02-21 12:29:50 +0530

    Added delete rest functionality

--- src/main/java/com/etoe/controllers/LocationRESTController.java
+++ src/main/java/com/etoe/controllers/LocationRESTController.java
@@ -3,7 +3,9 @@ package com.etoe.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,5 +35,10 @@ public class LocationRESTController {
 	@PutMapping
 	public Location updateLocation(@RequestBody Location location){
 		return locRepo.save(location);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteLocation(@PathVariable("id") Long id){
+		locRepo.deleteById(id);
 	}
 }
